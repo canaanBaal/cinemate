3ef0f89e88977d75c3d2ff3555a8b410e198dfdf
canaanBaal <johancore16@hotmail.com>
2023-03-30 20:20:05 -0300

    rel=noreferrer added

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -70,7 +70,7 @@ const MovieDetail = () => {
 
           <p className="my-4">
             <span className="mr-2 font-bold">IMDB Code:</span>
-            <a href={`https://www.imdb.com/title/${movie.imdb_id}`} target="_blank" rel="noreferer">{movie.imdb_id}</a>
+            <a href={`https://www.imdb.com/title/${movie.imdb_id}`} target="_blank" rel="noreferrer">{movie.imdb_id}</a>
           </p>
 
         </div>
